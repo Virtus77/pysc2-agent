b74734d6aafaca3de935f267c91c10b905e03565
Virtus77 <khd1326@naver.com>
2021-04-03 03:52:24 +0900

    Implement Building barracks

--- simple_agent.py
+++ simple_agent.py
@@ -5,16 +5,47 @@ from absl import app
 import random
 
 class SimpleAgent(base_agent.BaseAgent):
+    def unit_type_is_selected(self, obs, unit_type):
+    # checks both the single and multi-selections to see if the first selected unit is the correct type
+        if (len(obs.observation.single_select) > 0 and
+            obs.observation.single_select[0].unit_type == unit_type):
+            return True
+        if (len(obs.observation.multi_select) > 0 and
+            obs.observation.multi_select[0].unit_type == unit_type):
+            return True
+        return False
+    
+    def get_units_by_type(self, obs, unit_type):
+        return [unit for unit in obs.observation.feature_units
+                if unit.unit_type == unit_type]
+                
     def step(self, obs):
         super(SimpleAgent, self).step(obs)
+        
+        supply_depots = self.get_units_by_type(obs, units.Terran.SupplyDepot)
+        if len(supply_depots) == 0:
+            if self.unit_type_is_selected(obs, units.Terran.SCV):
+                if(actions.FUNCTIONS.Build_SupplyDepot_screen.id in
+                    obs.observation.available_actions):
+                    x = random.randint(0, 83)
+                    y = random.randint(0, 83)
+                    return actions.FUNCTIONS.Build_SupplyDepot_screen("now", (x, y))
+        
+        barracks = self.get_units_by_type(obs, units.Terran.Barracks)
+        if len(barracks) == 0 and len(barracks) < 3:
+           if self.unit_type_is_selected(obs, units.Terran.SCV):
+                if(actions.FUNCTIONS.Build_Barracks_screen.id in
+                    obs.observation.available_actions):
+                    x = random.randint(0, 83)
+                    y = random.randint(0, 83)
+                    return actions.FUNCTIONS.Build_Barracks_screen("now", (x,y))
         # get a list of all SCVs on the screen
-        scvs = [unit for unit in obs.observation.feature_units
-                if unit.unit_type == units.Terran.SCV]
+        scvs = self.get_units_by_type(obs, units.Terran.SCV)
         if len(scvs) > 0:
             scv = random.choice(scvs)
             return actions.FUNCTIONS.select_point("select_all_type", (scv.x, scv.y))
         return actions.FUNCTIONS.no_op()
-        
+    
 def main(unused_argv):
     agent = SimpleAgent()
     try:
